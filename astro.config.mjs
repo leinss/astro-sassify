0d1c3c41da1c716020409b5f8dbbbaa08db03223
Tobias Leinss <tobias@leinss.xyz>
2025-03-23 17:02:02 +0100

    Work on hero

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,16 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import alpinejs from '@astrojs/alpinejs';
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    tailwind(),
-    alpinejs()
-  ]
+  integrations: [tailwind(), alpinejs(), react()],
+  i18n: {
+    locales: ["en", "de"],
+    defaultLocale: "de",
+    routing: {
+      prefixDefaultLocale: false
+  }
+  }
 });
