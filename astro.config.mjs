2b02d10a2914ba77ad518e538fdcc13cbc00cd23
Tobias Leinss <tobias@leinss.xyz>
2025-03-31 16:01:35 +0200

    Make base variable

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,11 +8,7 @@ import react from "@astrojs/react"
 // https://astro.build/config
 export default defineConfig({
   site: "https://leinss-consulting.de",
-  base: "/ki",
   integrations: [tailwind(), alpinejs(), react()],
-  redirects: {
-    "/": "/ki",
-  },
   i18n: {
     locales: ["en", "de"],
     defaultLocale: "de",
