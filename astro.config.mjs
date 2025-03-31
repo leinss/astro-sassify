132b0d0b23c0810f4f57b10aea616d3298835f47
Tobias Leinss <tobias@leinss.xyz>
2025-03-31 14:53:07 +0200

    Change domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,23 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-import alpinejs from '@astrojs/alpinejs';
+import { defineConfig } from "astro/config"
+import tailwind from "@astrojs/tailwind"
+import alpinejs from "@astrojs/alpinejs"
 
-import react from "@astrojs/react";
+import react from "@astrojs/react"
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://leinss.xyz",
-  base: "/astro-sassify",
+  site: "https://leinss-consulting.de",
+  base: "/ki",
   integrations: [tailwind(), alpinejs(), react()],
+  redirects: {
+    "/": "/ki",
+  },
   i18n: {
     locales: ["en", "de"],
     defaultLocale: "de",
     routing: {
-      prefixDefaultLocale: false
-  }
-  }
-});
+      prefixDefaultLocale: false,
+    },
+  },
+})
