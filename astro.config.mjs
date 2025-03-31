67a7fc499dbc1d58aef115fdf93198ef4563a7d7
Tobias Leinss <tobias@leinss.xyz>
2025-03-31 17:30:05 +0200

    Add baseurl

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,11 @@
 import { defineConfig } from "astro/config"
 import tailwind from "@astrojs/tailwind"
 import alpinejs from "@astrojs/alpinejs"
-
 import react from "@astrojs/react"
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://leinss-consulting.de",
-  redirects: {},
   integrations: [tailwind(), alpinejs(), react()],
   i18n: {
     locales: ["en", "de"],
--- src/assets/config.ts
+++ src/assets/config.ts
@@ -1,5 +1,4 @@
 export default {
-  baseUrl: "",
   external_links: {
     strategy_call: "https://cal.com/tobias-leinss/strategiegespraech",
   },
