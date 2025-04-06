851309dd40cd31e8c14c665a195609001f55e8b1
Tobias Leinss <tobias@leinss.xyz>
2025-04-06 19:25:28 +0200

    Fix header buttons, add english strat meeting url

--- src/assets/config.ts
+++ src/assets/config.ts
@@ -1,5 +1,6 @@
 export default {
   external_links: {
-    strategy_call: "https://cal.com/tobias-leinss/strategiegespraech",
+    strategy_call_de: "https://cal.com/tobias-leinss/strategiegespraech",
+    strategy_call_en: "https://cal.com/tobias-leinss/strategymeeting",
   },
 };
